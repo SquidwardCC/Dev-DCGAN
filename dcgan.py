148fc78d302e963cea76960da3ff6b7118cf88ab
gingledoof <62526960+gingledoof@users.noreply.github.com>
2020-05-24 23:54:34 -0400

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -9,12 +9,11 @@ from torch.utils.tensorboard import SummaryWriter
 from model_utils import Discriminator, Generator
 
 lr = 0.0001
-batch_size = 9
+batch_size = 150
 image_size = 128
 channels_img = 3
 channels_noise = 256
-num_epochs = 10000
-
+num_epochs = 100000
 features_d = 32
 features_g = 32
 
@@ -24,7 +23,13 @@ my_transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,)),
     ])
 
-dataset = datasets.ImageFolder(root='datain/', transform=my_transform)
+def save():
+    torch.save(netD.state_dict(), f'checkpoints/netD')
+    torch.save(netG.state_dict(), f'checkpoints/netG')
+    torch.save(optimizerD.state_dict(), f'checkpoints/optD')
+    torch.save(optimizerG.state_dict(), f'checkpoints/optG')
+
+dataset = datasets.ImageFolder(root='C:\\Users\\MemeMaster\\Desktop\\r34_bot', transform=my_transform)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,6 +37,7 @@ print("cuda:", torch.cuda.is_available())
 
 netD = Discriminator(channels_img, features_d).to(device)
 netG = Generator(channels_noise, channels_img, features_g).to(device)
+
 
 optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(0.5, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(0.5, 0.999))
@@ -48,47 +54,60 @@ fixed_noise = torch.randn(64, channels_noise, 1, 1).to(device)
 writer_real = SummaryWriter(f'runs/bonk/test_real')
 writer_fake = SummaryWriter(f'runs/bonk/test_fake')
 
-print("Training")
+def train(resume=False, init_epoch=0):
+    print("Training")
 
-for epoch in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(dataloader):
-        data = data.to(device)
-        batch_size = data.shape[0]
+    if resume == True:
+        print("Loading from saved models...")
+        netD.load_state_dict(torch.load(f'checkpoints/netD'))
+        netG.load_state_dict(torch.load(f'checkpoints/netG'))
+        optimizerD.load_state_dict(torch.load(f'checkpoints/optD'))
+        optimizerG.load_state_dict(torch.load(f'checkpoints/optG'))
 
-        # Train discriminator
-        netD.zero_grad()
-        label = (torch.ones(batch_size)*0.9).to(device)
-        output = netD(data).reshape(-1)
-        lossD_real = criterion(output, label)
-        D_x = output.mean().item()
 
-        noise = torch.randn(batch_size, channels_noise, 1, 1,).to(device)
-        fake = netG(noise)
-        label = (torch.ones(batch_size)*0.1).to(device)
 
-        output = netD(fake.detach()).reshape(-1)
-        lossD_fake = criterion(output, label)
+    for epoch in range(num_epochs-init_epoch):
+        for batch_idx, (data, targets) in enumerate(dataloader):
+            data = data.to(device)
+            batch_size = data.shape[0]
 
-        lossD = lossD_real + lossD_fake
-        lossD.backward()
-        optimizerD.step()
+            # Train discriminator
+            netD.zero_grad()
+            label = (torch.ones(batch_size)*0.9).to(device)
+            output = netD(data).reshape(-1)
+            lossD_real = criterion(output, label)
+            D_x = output.mean().item()
 
-        # Train generator
-        netG.zero_grad()
-        label = torch.ones(batch_size).to(device)
-        output = netD(fake).reshape(-1)
-        lossG = criterion(output, label)
-        lossG.backward()
-        optimizerG.step()
+            noise = torch.randn(batch_size, channels_noise, 1, 1,).to(device)
+            fake = netG(noise)
+            label = (torch.ones(batch_size)*0.1).to(device)
 
-        if batch_idx % 5 == 0:
-            print(f'Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(dataloader)} \
-                  Loss D: {lossD:.4f}, loss G: {lossG:.4f} D(x): {D_x:.4f}')
+            output = netD(fake.detach()).reshape(-1)
+            lossD_fake = criterion(output, label)
 
-        with torch.no_grad():
-            fake = netG(fixed_noise)
+            lossD = lossD_real + lossD_fake
+            lossD.backward()
+            optimizerD.step()
 
-            img_grid_real = torchvision.utils.make_grid(data[:32], normalize=True)
-            img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
-            writer_real.add_image('Real Images', img_grid_real)
-            writer_real.add_image('Fake Images', img_grid_fake)
+            # Train generator
+            netG.zero_grad()
+            label = torch.ones(batch_size).to(device)
+            output = netD(fake).reshape(-1)
+            lossG = criterion(output, label)
+            lossG.backward()
+            optimizerG.step()
+
+            if batch_idx % 5 == 0:
+                print(f'Epoch [{epoch+init_epoch}/{num_epochs-init_epoch}] Batch {batch_idx}/{len(dataloader)} \
+                      Loss D: {lossD:.4f}, loss G: {lossG:.4f} D(x): {D_x:.4f}')
+
+            with torch.no_grad():
+                fake = netG(fixed_noise)
+
+                img_grid_real = torchvision.utils.make_grid(data[:32], normalize=True)
+                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+                writer_real.add_image('Real Images', img_grid_real)
+                writer_real.add_image('Fake Images', img_grid_fake)
+        save()
+
+train(resume=True, init_epoch=185)
